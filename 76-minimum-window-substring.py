d89642ae30b6f089e9bb08622adaadffe3cda24e
Anton Kanugalawattage <antondilon@gmail.com>
2020-10-09 07:15:26 -0400

    minimum-window-substring

---
+++ 76-minimum-window-substring.py
@@ -0,0 +1,79 @@
+# https://leetcode.com/problems/minimum-window-substring/
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        """ 
+        2 hashmaps for required and what you have
+
+        2 pointers left and right
+
+        2 vars of total chars found
+
+        move right check if need char
+            if need
+                ++ total chars found
+                add 1 to hashmap of char
+
+                check if total chars met
+                    if met
+                        end loop
+                        move to searching left
+                    else 
+                        R++
+            else 
+                R++
+
+        moving left
+            if this char in the need hashmap
+                found a satisfying
+                sub from hashmap and total found
+                L++
+                end loop
+                start moving right
+            else 
+                L++
+        """  
+            
+        required, have = {}, {}
+        left, right = 0, 0
+        totalReq, totalHave = len(t), 0
+        minSize = 999999
+        sL, sR = 0, 0
+        
+        for i in t:
+            if i in required: required[i] +=1
+            else: required[i] = 1
+                
+        for i in s:
+            if i in have: continue
+            else: have[i] = 0            
+
+        while (right < len(s) or left < len(s)):
+            if (totalReq == totalHave):
+                # Update smallest string
+                if (right - left < minSize):
+                    sL, sR = left, right
+                    minSize = right - left
+                    
+                # Moving left
+                if (s[left] in required):
+                    if (not have[s[left]] > required[s[left]]):
+                        totalHave-=1
+                    have[s[left]] -=1
+                left+=1
+                
+            # Moving right
+            elif (right < len(s)):
+                if (s[right] in required):
+                    if (have[s[right]] < required[s[right]]):
+                        totalHave+=1
+                    have[s[right]] +=1
+                right+=1
+
+            else: 
+                break
+             
+        return s[sL:sR]
+            
+            
