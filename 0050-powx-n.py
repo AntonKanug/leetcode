dcc17427d5e549023d98d5cf9244f353ef722abf
Anton Kanugalawattage <antondilon@gmail.com>
2021-07-21 05:42:50 -0400

    powx-n

---
+++ 0050-powx-n.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/powx-n/
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+            
+        if n == 0: return 1
+        if n < 0:
+            x = 1/x
+            n = -n
+        
+        if n % 2 == 1:
+            return x * self.myPow(x*x, n//2)
+        
+        return self.myPow(x*x, n//2) 
+    
