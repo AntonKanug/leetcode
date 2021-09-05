5a313823e2d7f8b52599c64a874c327b8f6c4cf1
Anton Kanugalawattage <antondilon@gmail.com>
2021-09-05 08:43:25 -0400

    rotting-oranges

---
+++ 994-rotting-oranges.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/rotting-oranges/submissions/
+
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        visited = set()
+        maxMin = 0        
+        oranges = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    oranges +=1
+                elif grid[i][j] == 2:
+                    # [x,y,minute]
+                    q.append([i,j,0])
+               
+        visitedOranges = set()
+        
+        while q:
+            i,j,m = q.popleft()
+            
+            if (i,j) in visited: continue
+            
+            visited.add((i,j))
+            maxMin = max(maxMin,m)
+            
+            for ver,hor in [[0,1],[1,0],[0,-1],[-1,0]]:
+                x,y = i+ver, j+hor
+                if not(0<=x<len(grid) and 0<=y<len(grid[0])): continue
+                if (x,y) not in visited and grid[x][y] == 1:
+                    q.append([x,y,m+1])
+                    visitedOranges.add((x,y))
+        
+        return maxMin if len(visitedOranges) == oranges else -1
+
+    
