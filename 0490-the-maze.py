5d5ddda08a47c4bff47b846322ec70b2f49e6518
Anton Kanugalawattage <antondilon@gmail.com>
2021-09-06 10:02:26 -0400

    the-maze

--- 0490-the-maze.py
+++ 0490-the-maze.py
@@ -10,16 +10,18 @@ class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
         q = deque()
         q.append(start)
+        maze[start[0]][start[1]] = -1
         
         while q:
             i,j = q.popleft()
 
             if [i,j] == destination: return True
-                
+             
             for x,y in [[0,1],[1,0],[0,-1],[-1,0,]]:
                 x1,y1 = x+i,y+j
                 
-                while 0<=x1<len(maze) and 0<=y1<len(maze[0]) and not maze[x1][y1]!=1: 
+                # Go to end of a wall for each direction
+                while 0<=x1<len(maze) and 0<=y1<len(maze[0]) and maze[x1][y1]!=1: 
                     x1 += x
                     y1 += y
     
@@ -27,8 +29,8 @@ class Solution:
                 y1 -= y
                 
                 if not maze[x1][y1]:
-                    q.append([x1,y1])
                     maze[x1][y1] = -1
+                    q.append([x1,y1])
                     
         return False
     
