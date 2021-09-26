592bf7de9a3ecf2e061a84601a6ab15b9ad50ecf
Anton Kanugalawattage <antondilon@gmail.com>
2021-09-26 05:56:31 -0400

    random-pick-with-weight

---
+++ 0528-random-pick-with-weight.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/random-pick-with-weight/
+
+class Solution:
+    def __init__(self, w: List[int]):
+        self.w = w
+        for i in range(1,len(self.w)):
+            self.w[i] += self.w[i-1]
+        
+    def pickIndex(self) -> int:
+        val = random.randint(1,self.w[-1])
+        
+        l,r = 0, len(self.w)
+        
+        while l<r:
+            mid = l + (r-l)//2
+            
+            if self.w[mid] == val: return mid
+            elif self.w[mid] < val: l = mid + 1
+            else: r = mid
+                
+        return l
+        
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
