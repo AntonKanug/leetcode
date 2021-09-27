6e1d7c18651f2bdb023f73a147202e4e8dd3beee
Anton Kanugalawattage <antondilon@gmail.com>
2021-09-27 12:03:44 -0400

    simplify-path

---
+++ 0071-simplify-path.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/simplify-path/submissions/
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        
+        for i in path.split('/'):
+            if i == '..':
+                if stack:
+                    stack.pop()
+            elif i and i != '.':
+                stack.append(i)
+                
+        return '/' + '/'.join(stack)
+    
